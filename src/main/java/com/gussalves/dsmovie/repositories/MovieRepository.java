7802080a435f9b241199d34ec3ca3ca1a9d13fad
gusal <gus.alves.araujo@gmail.com>
2022-01-24 23:57:08 -0300

    Deploy heroku

---
+++ src/main/java/com/gussalves/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.gussalves.dsmovie.repositories;
+
+import com.gussalves.dsmovie.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
